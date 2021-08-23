ac48bfb41b521614bd136d7c0d4e4663c31e137b
Dariusz Raba <rabadariusz@gmail.com>
2021-08-23 22:23:32 +0200

    add 5th Test

--- src/test/java/com/tddexercise/GreetingsTest.java
+++ src/test/java/com/tddexercise/GreetingsTest.java
@@ -8,10 +8,12 @@ import static org.junit.jupiter.api.Assertions.*;
 public class GreetingsTest {
 
     private Greetings greetings;
+
     @BeforeEach
-    public void beforeEachTestMethod(){
+    public void beforeEachTestMethod() {
         this.greetings = new Greetings();
     }
+
     @Test
     public void checkInterpolation() {
         //given
@@ -24,34 +26,53 @@ public class GreetingsTest {
     }
 
     @Test
-    public void checkNullValue(){
+    public void checkNullValue() {
         //given
         String shouldReturn = "Hello, my friend.";
         //when
         String nullValue = null;
         String sayHello = greetings.greet(nullValue);
         //then
-        assertEquals(shouldReturn,sayHello);
+        assertEquals(shouldReturn, sayHello);
     }
 
     @Test
-    public void checkShoutingBack(){
+    public void checkShoutingBack() {
         //given
         String shouldReturn = "HELLO, JERRY!";
         //when
         String shoutBack = greetings.greet("JERRY");
         //then
-        assertEquals(shouldReturn,shoutBack);
+        assertEquals(shouldReturn, shoutBack);
     }
 
     @Test
-    public void checkTwoNamesGreeting(){
+    public void checkTwoNamesGreeting() {
         //given
         String[] names = {"Jill", "Jane"};
         String shouldReturn = "Hello, Jill and Jane";
         //when
         String greeting = greetings.greet(names);
         //then
-        assertEquals(shouldReturn,greeting);
+        assertEquals(shouldReturn, greeting);
+    }
+
+    @Test
+    public void checkMoreThenTwoNamesHandling() {
+        //given
+        String[] threeNames = {"Jill", "Jane", "Judy"};
+        String[] fourNames = {"Jill", "Jane", "Judy", "Michael"};
+        String[] fiveNames = {"Jill", "Jane", "Judy", "Michael", "Nicole"};
+        String shouldReturnforThree = "Hello, Jill, Jane and Judy.";
+        String shouldReturnforFour = "Hello, Jill, Jane, Judy and Michael.";
+        String shouldReturnforFive = "Hello, Jill, Jane, Judy, Michale and Nicole.";
+        //when
+        final String threeGreet = greetings.greet(threeNames);
+        final String fourGreet = greetings.greet(fourNames);
+        final String fiveGreet = greetings.greet(fiveNames);
+        //then
+        assertAll(() -> assertEquals(shouldReturnforThree, threeGreet),
+                () -> assertEquals(shouldReturnforFour, fourGreet),
+                () -> assertEquals(shouldReturnforFive, fiveGreet));
     }
 }
