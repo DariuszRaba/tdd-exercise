0f8a19fe621cc9b25a1a9433ec005387f2cf096d
Dariusz Raba <rabadariusz@gmail.com>
2021-08-30 19:24:44 +0200

    add 3rd StringCalculator TDD test

--- src/test/java/com/tddexercise/StringCalculatorTest.java
+++ src/test/java/com/tddexercise/StringCalculatorTest.java
@@ -10,12 +10,12 @@ class StringCalculatorTest {
     private StringCalculator stringCalculator;
 
     @BeforeEach
-    public void beforeEachMethodTest(){
+    public void beforeEachMethodTest() {
         this.stringCalculator = new StringCalculator();
     }
 
     @Test
-    public void Sum_Arguments_Variants_Test(){
+    public void Sum_Arguments_Variants_Test() {
         //given
         String emptyString = "";
         String oneArgument = "1";
@@ -28,13 +28,14 @@ class StringCalculatorTest {
         final int oneArgumentSum = stringCalculator.add(oneArgument);
         final int twoArgumentsSum = stringCalculator.add(twoArguments);
         //then
-        assertAll(()-> assertEquals(shouldReturnForEmptyString,emptySum),
-                ()-> assertEquals(shouldReturnForOneArgument, oneArgumentSum),
-                ()-> assertEquals(shouldReturnForTwoArguments, twoArgumentsSum));
+        assertAll(() -> assertEquals(shouldReturnForEmptyString, emptySum),
+                () -> assertEquals(shouldReturnForOneArgument, oneArgumentSum),
+                () -> assertEquals(shouldReturnForTwoArguments, twoArgumentsSum));
 
     }
+
     @Test
-    public void Sum_Multiple_Numbers(){
+    public void Sum_Multiple_Numbers() {
         //given
         String numbers1 = "321,4342,432,121,3";
         String numbers2 = "1,442,223,11,31,164,686,";
@@ -47,10 +48,28 @@ class StringCalculatorTest {
         final int sumOfNumbers2 = stringCalculator.add(numbers2);
         final int sumOfNumbers3 = stringCalculator.add(numbers3);
         //then
-        assertAll(()-> assertEquals(shouldReturnForNumbers1,sumOfNumbers1),
-                ()-> assertEquals(shouldReturnForNumbers2, sumOfNumbers2),
-                ()-> assertEquals(shouldReturnForNumbers3, sumOfNumbers3));
+        assertAll(() -> assertEquals(shouldReturnForNumbers1, sumOfNumbers1),
+                () -> assertEquals(shouldReturnForNumbers2, sumOfNumbers2),
+                () -> assertEquals(shouldReturnForNumbers3, sumOfNumbers3));
 
+    }
+
+    @Test
+    public void Sum_Numbers_Separated_With_New_Line() {
+        //given
+        String numbers1 = "1\n2,3";
+        String numbers2 = "1,2\n3";
+        String numbers3 = "1\n2\n3";
+        int shouldReturn = 6;
+        //when
+        final int sumOfNumbers1 = stringCalculator.add(numbers1);
+        final int sumOfNumbers2 = stringCalculator.add(numbers2);
+        final int sumOfNumbers3 = stringCalculator.add(numbers3);
+        //then
+        assertAll(
+                () -> assertEquals(shouldReturn, sumOfNumbers1),
+                () -> assertEquals(shouldReturn, sumOfNumbers2),
+                () -> assertEquals(shouldReturn, sumOfNumbers3));
     }
 
 }
