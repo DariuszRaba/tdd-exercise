a54897d88c1d5f36b2fce82dc7856f071ca09327
Dariusz Raba <rabadariusz@gmail.com>
2021-08-30 20:01:56 +0200

    add 4th StringCalculator requirement feature

--- src/main/java/com/tddexercise/StringCalculator.java
+++ src/main/java/com/tddexercise/StringCalculator.java
@@ -5,6 +5,9 @@ import java.util.Arrays;
 public class StringCalculator {
     public int add(String numbers) {
         final int numbersLength = numbers.length();
+        if(numbers.startsWith("//")){
+            return sumNumbersWithCustomDelimiter(numbers);
+        }
         switch (numbersLength) {
             case 0:
                 return 0;
@@ -12,5 +15,11 @@ public class StringCalculator {
                 return Integer.parseInt(numbers);
         }
         return Arrays.stream(numbers.split("[,\n]")).mapToInt(Integer::parseInt).sum();
+    }
+
+    private int sumNumbersWithCustomDelimiter(String numbers) {
+        final int endOfDelimiter = numbers.indexOf("\n");
+        String delimiter = numbers.substring(2,endOfDelimiter);
+        return Arrays.stream(numbers.substring(endOfDelimiter+1).split(delimiter)).mapToInt(Integer::parseInt).sum();
     }
 }
