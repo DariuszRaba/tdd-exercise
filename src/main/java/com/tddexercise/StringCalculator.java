67f87efcd8c9d09c4dbdb7f368847687e5978c05
Dariusz Raba <rabadariusz@gmail.com>
2021-08-31 16:12:44 +0200

    add negatives requirement feature and code refactor

--- src/main/java/com/tddexercise/StringCalculator.java
+++ src/main/java/com/tddexercise/StringCalculator.java
@@ -1,25 +1,45 @@
 package com.tddexercise;
 
 import java.util.Arrays;
+import java.util.regex.Pattern;
 
 public class StringCalculator {
-    public int add(String numbers) {
-        final int numbersLength = numbers.length();
-        if(numbers.startsWith("//")){
+
+    public int add(String numbers) throws Exception {
+        if (numbers.startsWith("//")) {
             return sumNumbersWithCustomDelimiter(numbers);
         }
-        switch (numbersLength) {
-            case 0:
-                return 0;
-            case 1:
-                return Integer.parseInt(numbers);
+        if (numbers.length() < 1) {
+            return 0;
+        } else if (numbers.length() == 1) {
+            return Integer.parseInt(numbers);
+        } else if (containNegativeNumber(numbers)) {
+            throw new Exception(String.format("negatives not allowed [%s]", getNegativesFromInput(numbers.split("[,\n]"))));
+        } else {
+            return Arrays.stream(numbers.split("[,\n]")).mapToInt(Integer::parseInt).sum();
         }
-        return Arrays.stream(numbers.split("[,\n]")).mapToInt(Integer::parseInt).sum();
     }
 
-    private int sumNumbersWithCustomDelimiter(String numbers) {
+    private int sumNumbersWithCustomDelimiter(String numbers) throws Exception {
         final int endOfDelimiter = numbers.indexOf("\n");
-        String delimiter = numbers.substring(2,endOfDelimiter);
-        return Arrays.stream(numbers.substring(endOfDelimiter+1).split(delimiter)).mapToInt(Integer::parseInt).sum();
+        String delimiter = numbers.substring(2, endOfDelimiter);
+        final String[] numbersArr = numbers.substring(endOfDelimiter + 1).split(delimiter);
+        if (containNegativeNumber(numbers)) {
+            throw new Exception(String.format("negatives not allowed [%s]", getNegativesFromInput(numbersArr)));
+        }
+        return Arrays.stream(numbersArr).mapToInt(Integer::parseInt).sum();
+    }
+
+    private boolean containNegativeNumber(String numbers) {
+        return Pattern.compile("-[1-9]").matcher(numbers).find();
+    }
+
+    private String getNegativesFromInput(String[] numbers) {
+        final StringBuilder sb = new StringBuilder("");
+        for (String number : numbers
+        ) {
+            if (Integer.parseInt(number) < 0) sb.append(number).append(",");
+        }
+        return sb.substring(0, sb.length() - 1);
     }
 }
